1ad4963e13c609e76bb290bac09911ca63454731
FAITH <OnlyMeHipnoz@hotmail.com>
2020-05-01 16:27:05 +0700

    update source files

--- source/main.c
+++ source/main.c
@@ -62,10 +62,11 @@ int main(int argc, char**argv) {
 	gtk_window_set_title(GTK_WINDOW(window), "INCOMES AND EXPENSES RECORDS"); //program's title
 	icon = create_pixbuf("share/icon/Emo01.png"); //Set icon
 	gtk_window_set_icon(GTK_WINDOW(window), icon); //Sets up the icon representing a GtkWindow.
-	layout = gtk_layout_new(NULL, NULL); //Creates a new GtkLayout.  
+	layout = gtk_layout_new(NULL, NULL); //Creates a new GtkLayout.
 	load_ui(); //call load ui function
 	gtk_container_add(GTK_WINDOW(window), layout); //Adds widget to container
 	get_data_from_sql();
+	create_sql();
 	gtk_widget_show_all(window); //Recursively shows a widget, and any child widgets
 	gtk_main(); //runs the main loop
 	return 0;
@@ -99,19 +100,19 @@ static GtkWidget *create_tree_view(void) {
 	gtk_tree_view_column_set_title(col3, "Incomes");
 	gtk_tree_view_column_set_title(col4, "Expenses");
 	gtk_tree_view_column_set_title(col5, "Summary");
-	
+
 	gtk_tree_view_append_column(GTK_TREE_VIEW(treeview), col1); //set column to tree view column 1-5
 	gtk_tree_view_append_column(GTK_TREE_VIEW(treeview), col2);
 	gtk_tree_view_append_column(GTK_TREE_VIEW(treeview), col3);
 	gtk_tree_view_append_column(GTK_TREE_VIEW(treeview), col4);
 	gtk_tree_view_append_column(GTK_TREE_VIEW(treeview), col5);
-	
+
 	gtk_tree_view_column_set_fixed_width(col1, 100); //Set tree width column 1-5
 	gtk_tree_view_column_set_fixed_width(col2, 350);
 	gtk_tree_view_column_set_fixed_width(col3, 110);
 	gtk_tree_view_column_set_fixed_width(col4, 110);
 	gtk_tree_view_column_set_fixed_width(col5, 110);
-	
+
 	cell1 = gtk_cell_renderer_text_new(); //create cell
 	cell2 = gtk_cell_renderer_text_new();
 	cell3 = gtk_cell_renderer_text_new();
@@ -129,18 +130,18 @@ static GtkWidget *create_tree_view(void) {
 	gtk_tree_view_column_add_attribute(col3, cell3, "text", incomes);
 	gtk_tree_view_column_add_attribute(col4, cell4, "text", expenses);
 	gtk_tree_view_column_add_attribute(col5, cell5, "text", summary);
-	
+
 	model = create_model();
 	gtk_tree_view_set_model(GTK_TREE_VIEW(treeview), model);
 	g_object_unref(model);
-	
+
 	return treeview;
 }
 
 static void load_ui(void) {
 	GtkWidget *income_label, *expense_label, *note_label;
 	load_css();
-	
+
 	income_label = gtk_label_new("Incomes: ");
 	expense_label = gtk_label_new("Expenses: ");
 	note_label = gtk_label_new("Notes: ");
@@ -149,7 +150,7 @@ static void load_ui(void) {
 	gtk_widget_set_name(expense_label, "expense_label");
 	gtk_widget_set_name(note_label, "note_label");
 
-	gtk_widget_set_size_request(GTK_LABEL(income_label), 50, 50); //Sets the minimum size of a widget. 
+	gtk_widget_set_size_request(GTK_LABEL(income_label), 50, 50); //Sets the minimum size of a widget.
 	gtk_widget_set_size_request(GTK_LABEL(expense_label), 50, 50);
 	gtk_widget_set_size_request(GTK_LABEL(note_label), 50, 50);
 
@@ -160,14 +161,14 @@ static void load_ui(void) {
 	income = gtk_entry_new(); //Creates a new entry.
 	expense = gtk_entry_new(); //Creates a new entry.
 	note = gtk_entry_new(); //Creates a new entry.
-	
+
 	gtk_widget_set_name(income, "income_input"); //refer them from CSS file
 	gtk_widget_set_name(expense, "expense_input");
 	gtk_widget_set_name(note, "note_input");
-	
+
 	gtk_widget_set_size_request(GTK_ENTRY(income), 400, 50); //Sets the minimum size of a widget
 	gtk_widget_set_size_request(GTK_ENTRY(expense), 400, 50);
-	gtk_widget_set_size_request(GTK_ENTRY(note), 400, 100); 
+	gtk_widget_set_size_request(GTK_ENTRY(note), 400, 100);
 
 	gtk_entry_set_placeholder_text(GTK_ENTRY(income), "Incomes"); //Sets text to be displayed in entry when it is empty and unfocused
 	gtk_entry_set_placeholder_text(GTK_ENTRY(expense), "Expenses");
@@ -182,18 +183,18 @@ static void load_ui(void) {
 	gtk_layout_put(GTK_LAYOUT(layout), note, 320, 415); //Adds child_widget to layout , at position (320 , 415).
 
 	submit_btn = gtk_button_new_with_label("Submit"); //Creates a Submit GtkButton widget
-	gtk_widget_set_size_request(GTK_BUTTON(submit_btn), 100, 50); //Set size of Submit Button 
-	gtk_layout_put(GTK_LAYOUT(layout), submit_btn, 320, 517); //Adds Submit Button to layout 
+	gtk_widget_set_size_request(GTK_BUTTON(submit_btn), 100, 50); //Set size of Submit Button
+	gtk_layout_put(GTK_LAYOUT(layout), submit_btn, 320, 517); //Adds Submit Button to layout
 	g_signal_connect(submit_btn, "clicked", G_CALLBACK(add_data_to_sql), NULL);
 
 	delete_btn = gtk_button_new_with_label("Delete"); //Creates a Delete GtkButton widget
-	gtk_widget_set_size_request(GTK_BUTTON(delete_btn), 100, 50); //Set size of Delete Button 
-	gtk_layout_put(GTK_LAYOUT(layout), delete_btn, 425, 517); ////Adds Delete Button to layout 
+	gtk_widget_set_size_request(GTK_BUTTON(delete_btn), 100, 50); //Set size of Delete Button
+	gtk_layout_put(GTK_LAYOUT(layout), delete_btn, 425, 517); ////Adds Delete Button to layout
 	g_signal_connect(delete_btn, "clicked", G_CALLBACK(delete_row), NULL);
 
 	sum_btn = gtk_button_new_with_label("Summary"); //Creates a Summary GtkButton widget
-	gtk_widget_set_size_request(GTK_BUTTON(sum_btn), 100, 50); //Set size of Summary Button 
-	gtk_layout_put(GTK_LAYOUT(layout), sum_btn, 530, 517); //Adds Summary Button to layout 
+	gtk_widget_set_size_request(GTK_BUTTON(sum_btn), 100, 50); //Set size of Summary Button
+	gtk_layout_put(GTK_LAYOUT(layout), sum_btn, 530, 517); //Adds Summary Button to layout
 	g_signal_connect(sum_btn, "clicked", G_CALLBACK(get_sum_from_sql), NULL);
 	//g_signal_connect(sum_btn, "clicked", G_CALLBACK(get_data_from_tree_view), NULL);
 
@@ -205,7 +206,7 @@ static void load_ui(void) {
 	gtk_widget_set_size_request(GTK_CALENDAR(calendar), 200, 100);
 	gtk_layout_put(GTK_LAYOUT(layout), GTK_CALENDAR(calendar), 10, 315);
 	g_signal_connect(calendar, "day-selected", G_CALLBACK(create_sql), NULL);
-	
+
 	scroll = gtk_scrolled_window_new(NULL, NULL); //Creates a new scrolled window
 	gtk_widget_set_size_request(GTK_SCROLLED_WINDOW(scroll), 780, 300);
 	gtk_layout_put(GTK_LAYOUT(layout), scroll, 10, 10);
@@ -214,7 +215,7 @@ static void load_ui(void) {
 	gtk_tree_view_set_enable_search(treeview, FALSE);
 	gtk_widget_set_size_request(GTK_TREE_VIEW(treeview), 770, 300);
 	gtk_container_add(GTK_SCROLLED_WINDOW(scroll), treeview);
-
+	g_signal_connect(treeview, "key-press-event", G_CALLBACK(keypress_listener), NULL);
 	gtk_widget_set_name(treeview, "treeview");
 }
 
@@ -273,7 +274,7 @@ int add_data_to_sql(void) {
 	char a[100], b[100], c[100], d[100], result[100], check1[100], check2[100];
 	double income_value, expense_value;
 	int len1 = 1, len2 = 1;
-	snprintf(check1, sizeof(check1), "%s", gtk_entry_get_text(GTK_ENTRY(income))); 
+	snprintf(check1, sizeof(check1), "%s", gtk_entry_get_text(GTK_ENTRY(income)));
 	snprintf(check2, sizeof(check2), "%s", gtk_entry_get_text(GTK_ENTRY(expense)));
 
 	for (int i = 0; i < strlen(check1); i++) {
@@ -289,28 +290,28 @@ int add_data_to_sql(void) {
 	else {
 		printf("Passed\n");
 	}
-	
+
 	snprintf(b, sizeof(b), "%s", gtk_entry_get_text(GTK_ENTRY(note)));
 	income_value = atof((char *)gtk_entry_get_text(GTK_ENTRY(income)));
 	expense_value = atof((char *)gtk_entry_get_text(GTK_ENTRY(expense)));
 	snprintf(c, sizeof(c), "%.2lf", income_value);
 	snprintf(d, sizeof(d), "%.2lf", expense_value);
 	snprintf(result, sizeof(result), "%.2lf", (income_value - expense_value));
-	
+
 	int rc = sqlite3_open(database, &db);
 	char *sql = &date_format;
-	rc = sqlite3_prepare_v2(db, sql, -1, &statement, 0); //Prepare the SQL statement 
-	
-	sqlite3_bind_text(statement, 1, b, -1, 0); 
+	rc = sqlite3_prepare_v2(db, sql, -1, &statement, 0); //Prepare the SQL statement
+
+	sqlite3_bind_text(statement, 1, b, -1, 0);
 	sqlite3_bind_text(statement, 2, c, -1, 0);
 	sqlite3_bind_text(statement, 3, d, -1, 0);
 	sqlite3_bind_text(statement, 4, result, -1, 0);
-	
+
 	printf("%s", sql);
 	int step = sqlite3_step(statement); //called to evaluate the statement
-	get_sum_from_sql(); 
+	get_sum_from_sql();
 	gtk_entry_set_text(GTK_ENTRY(note), ""); //Sets the text in the widget to the given value, replacing the current contents
-	gtk_entry_set_text(GTK_ENTRY(income), ""); 
+	gtk_entry_set_text(GTK_ENTRY(income), "");
 	gtk_entry_set_text(GTK_ENTRY(expense), "");
 }
 
@@ -333,7 +334,7 @@ int callback(void *notused, int argc, char **argv, char **colname) {
 	char date_format[200];
 	gtk_calendar_get_date(GTK_CALENDAR(calendar), &year, &month, &day);
 	snprintf(date_format, sizeof(date_format), "%02d/%02d/%04d", day, month + 1, year);
-	
+
 	notused = 0;
 
 	//char buffer[200];
@@ -341,7 +342,7 @@ int callback(void *notused, int argc, char **argv, char **colname) {
 	//printf("%s\n", buffer);
 	gtk_tree_store_append(treestore, &toplevel, NULL); //Appends a new row to treestore
 	gtk_tree_store_set(treestore, &toplevel, 0, date_format, -1); //Sets the value of one or more cells in the row referenced by iter
-	
+
 	for (int i = 0; i < argc; i++) {
 		gtk_tree_store_set(treestore, &toplevel, i+1, argv[i], -1);
 	}
@@ -507,6 +508,9 @@ int keypress_listener(GtkWidget *button, GdkEventKey *event) {
 	snprintf(a, sizeof(a), "%s", gdk_keyval_name(event->keyval));
 	if (event != NULL && !(strcmp(a, "Return"))) {
 		add_data_to_sql();
+	}
+	else if (event != NULL && !(strcmp(a, "Delete"))) {
+		delete_row();
 	}
 	else {
 		return 0;
